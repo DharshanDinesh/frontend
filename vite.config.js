e92d5da2f8338ce59ce4c0a3410bb55f70f6e81c
Dharshan <dharshandinesh309@gmail.com>
2024-07-16 15:32:26 +0530

    asdf

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,29 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: (id) => {
-          if (id.includes('node_modules')) {
-            // Split dependencies into separate chunks
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
-          }
-        }
-      },
-    },
-    chunkSizeWarningLimit: 1500, // Set the chunk size limit to 1500 KB (1.5 MB)
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
 
+  return {
+    plugins: [react()],
+    server: {
+      host: '0.0.0.0',
+      port: parseInt(env.VITE_PORT) || 3000,
+    },
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: (id) => {
+            if (id.includes('node_modules')) {
+              // Split dependencies into separate chunks
+              return id.toString().split('node_modules/')[1].split('/')[0].toString();
+            }
+          }
+        },
+      },
+      chunkSizeWarningLimit: 1500, // Set the chunk size limit to 1500 KB (1.5 MB)
+    },
+
+  }
 })
